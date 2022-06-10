ba704ea7f3608f2c89660f2f0b06080fb37cb16f
Andrey B <aib98339@gmail.com>
2022-06-10 11:47:49 +0300

    Исключения

--- Exception.java
+++ Exception.java
@@ -1,19 +1,20 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
+
+import java.io.*;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Exception {
-    public static void main(String [] args) throws ArithmeticException {
+    public static void main(String [] args) throws InputMismatchException, IOException {
         OneException d = new OneException();
         int excep;
         int c = 8;
-        int [] a = {4, 8, 16, 14, 64};
-        int [] b = {2, 4, 0, 2, 4};
-        for (int i = 0;  i < a.length; i++) {
-            excep = d.number(a [i], b [i]);
+        int[] a = {4, 8, 16, 14, 64};
+        int[] b = {2, 4, 0, 2, 4};
+        for (int i = 0; i < a.length; i++) {
+            excep = d.number(a[i], b[i]);
             System.out.println(excep);
         }
         System.out.println();
@@ -26,24 +27,22 @@ public class Exception {
             Scanner scanner = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            System.out.println("Файл не найден ");
+            System.out.println("Файл не найден " + e.getMessage());
         }
 
-        try {// Доработать
+//
+        //Используeм rethrow - вызов повторной ошибки
+        try {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Необходимо вести число: ");
             int pushKey = scanner.nextInt();
-        }catch (ArithmeticException e){
-            System.out.println("Необходимо еще раз ввести цифры");
-        }
-
-
-        try {
             System.out.println("Вы указали следующее число: " + pushKey);
-        } catch (ArithmeticException e) {
-            System.out.println("Прошу использовать цифры: ");
-            throw e;
+        } catch (InputMismatchException e) {
+            System.out.println("Необходимо использовать цифры и заново запустить программу: ");
+           throw e;
         }
+
+
 
 
 
@@ -56,6 +55,8 @@ public class Exception {
 //        main(args);
 
     }
+
+
 
 }
 
